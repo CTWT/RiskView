94a442beae20917ef010f76acdce7a606845c02b
setia <inchul9603@naver.com>
2025-08-29 12:55:23 +0900

    docs : db접근 테스트 파일 수정
    db터널링 서버 접근 방식 수정 및 접근 테스트 완료
    
    신인철 | 25.08.29

--- ai-api/api/dbConnect_Test.py
+++ ai-api/api/dbConnect_Test.py
@@ -2,15 +2,25 @@ import mysql.connector
 from dotenv import load_dotenv
 import os 
 
+#  수업명 : 가비아 2회차
+#  이름 : 신인철
+#  작성자 : 신인철
+#  수정자 : 
+#  작성일 : 25.07.30
+#  파일명 : dbConnect_Test.py
+
+# Python mysql DB connection Test source 
+
 def test_mysql_connection() :
     load_dotenv()
     
     try : 
         conn = mysql.connector.connect(
-            host = os.getenv('host'),
-            user = os.getenv('user'),
-            password = os.getenv('password'),
-            database = os.getenv('database'),
+            host = os.getenv('DB_HOST'),
+            port = os.getenv('DB_PORT'),
+            user = os.getenv('DB_USER'),
+            password = os.getenv('DB_PASSWORD'),
+            database = os.getenv('DB_NAME'),
         )
         
         if conn.is_connected() :

2c8fe8ab6dc38264a7f86a5683023fda893a96c4
hogwan <asdf36597@naver.com>
2025-09-22 15:13:49 +0900

    fix: 계약일 없을 때 오류뜨던 것 수정
    김관호 | 2025.09.22

--- backend/src/main/java/realty/controller/ContractController.java
+++ backend/src/main/java/realty/controller/ContractController.java
@@ -23,6 +23,7 @@ import realty.apicommunication.FileComponent;
 import realty.apicommunication.MapComponent;
 import realty.apicommunication.OcrComponent;
 import realty.domain.dto.AiRiskAnalysisRequest;
+import realty.domain.dto.AnalysisSummaryDTO;
 import realty.domain.dto.AnomalyDetectResult;
 import realty.domain.dto.ContractClauseDTO;
 import realty.domain.dto.ContractDTO;
@@ -98,6 +99,17 @@ public class ContractController {
                 .body(dto);
     }
 
+    @GetMapping("/analysisSummary")
+    public ResponseEntity<AnalysisSummaryDTO> getSummary(
+        @RequestParam("documentCode") String documentCode) {
+            AnalysisSummaryDTO analysisSummaryDTO = contractService.getAnalysisSummary(documentCode);
+
+            return ResponseEntity
+                    .ok()
+                    .body(analysisSummaryDTO);
+
+    }
+
     /**
      * 파일 업로드 후 OCR 실행
      */
@@ -108,7 +120,7 @@ public class ContractController {
         // 계약서 정보
         fileComponent.saveTmpFile(file, session);
         ContractDTO.ContractInfo contractInfo = ocrComponent.scanContract(file);
-        log.info("컨트렉트데이트 ==> {}", contractInfo.getStructuredContractDataDTO().getContractDate().toString());
+        //log.info("컨트렉트데이트 ==> {}", contractInfo.getStructuredContractDataDTO().getContractDate().toString());
 
         // 맵 정보
         String address = contractInfo.getStructuredContractDataDTO().getLocation();

4b9605dec6570f2f62346ea56019e3615ec9168e
setia <inchul9603@naver.com>
2025-09-28 03:55:33 +0900

    fix : 삭제 코드 복구
    삭제 코드 복구
    
    신인철 | 25.09.28

--- frontend/realty/src/components/ui/CommonContainerHeader.tsx
+++ frontend/realty/src/components/ui/CommonContainerHeader.tsx
@@ -13,16 +13,19 @@ import "../components.css";
 interface CommonContainerHeaderProps {
     subtitle: string;
     title: string;
+    description: string;
 }
 
 const CommonContainerHeader = ({
     subtitle,
     title,
+    description,
 }: CommonContainerHeaderProps) => {
     return (
         <div className="common-container-header">
             <h2 className="common-container-header-subtitle">{subtitle}</h2>
             <h1 className="common-container-header-title">{title}</h1>
+            <p className="common-container-header-description">{description}</p>
         </div>
     );
 };

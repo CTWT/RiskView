5dcaa20e796acaf0eb8a5041e309629e22d065d2
setia <inchul9603@naver.com>
2025-09-28 06:18:17 +0900

    docs : CORS설정 추가
    배포용 CORS설정 추가
    
    신인철 | 25.09.28

--- backend/src/main/java/realty/config/WebConfig.java
+++ backend/src/main/java/realty/config/WebConfig.java
@@ -29,7 +29,8 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")  // 모든 경로에 대해
                                 .allowedOrigins(
                     "http://localhost:5173",   // 로컬 개발 환경
-                    "http://1.201.19.40"       // 배포 서버 환경
+                    "http://1.201.19.40",           // 배포 서버 환경
+                    "http://1.201.19.40:8080"       // 배포 서버 환경
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")

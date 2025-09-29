c5e76d3d89429b9d5029d2077045fd9203ea8725
setia <inchul9603@naver.com>
2025-09-29 14:17:54 +0900

    docs : requestmapping 추가
    추가
    
    신인철 | 25.09.29

--- backend/src/main/java/realty/controller/ContractController.java
+++ backend/src/main/java/realty/controller/ContractController.java
@@ -16,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -56,6 +57,7 @@ import realty.support.AddressFormatter;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class ContractController {
     private static final Logger log = LoggerFactory.getLogger(ContractController.class);
 
